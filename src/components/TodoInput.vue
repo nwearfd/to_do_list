139b9d8306da095a9f649bde0c3ba96fbd6ae596
nwearfd <wearfd031013@naver.com>
2021-10-30 18:44:53 +0900

    to_do_list

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoitem" placeholder="할 일을 적으세요" v-on:keyup.enter="addTodo" />
+        <span class="addContainer" v-on:click="addTodo">
+            <font-awesome-icon class="addBtn" icon="plus" aria-hidden="true" />
+        </span>
+
+        <modal v-if="showModal" @close="showModal = false">
+            <h3 slot="header">경고</h3>
+            <span slot="body">할 일 선택</span>
+            <button slot="footer" @click="showModal = false">닫기</button>
+        </modal>
+    </div>
+</template>
+
+<script>
+    export default{};
+</script>
+
+<style scoped>
+
+</style>
